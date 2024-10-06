42d02d1e03223f3939cdd4c6f69dab65663915ae
icewolfz <icewolfz@users.noreply.github.com>
2024-10-06 18:15:44 -0500

    Fix some of my custom sm package to better handle telnet connections

--- src/packages/sm/sm.cc
+++ src/packages/sm/sm.cc
@@ -2,34 +2,43 @@
 #include <event2/buffer.h>       // for evbuffer_freeze, etc
 #include <event2/bufferevent.h>  // for bufferevent_enable, etc
 #include "net/websocket.h"
+#include "thirdparty/libtelnet/libtelnet.h"
+#include "net/ws_telnet.h"
 
 #ifdef F_RECEIVE_BINARY
 void  f_receive_binary (void)
 {
-    if (sp->type == T_STRING) {
-        if (current_object->interactive) {
-            int len = SVALUE_STRLEN(sp);
-
-            if (len > LARGEST_PRINTABLE_STRING)
-                error("Printable strings limited to length of %d.\n", LARGEST_PRINTABLE_STRING);
-            if(current_object->interactive->connection_type == PORT_TYPE_WEBSOCKET)
-                websocket_send_text(current_object->interactive->lws, sp->u.string, len);
+    interactive_t *ip = current_object->interactive;
+    if(!ip) return;
+    if (sp->type == T_STRING) {     
+        int len = SVALUE_STRLEN(sp);  
+        if (len > LARGEST_PRINTABLE_STRING)
+            error("Printable strings limited to length of %d.\n", LARGEST_PRINTABLE_STRING);
+        if(ip->connection_type == PORT_TYPE_WEBSOCKET)  {
+            if(lws_get_protocol(ip->lws)->id == PROTOCOL_WS_TELNET)
+                telnet_send(ip->telnet, sp->u.string, len);
             else
-                bufferevent_write(current_object->interactive->ev_buffer, sp->u.string, len);                
+                websocket_send_text(ip->lws, sp->u.string, len);
         }
-        free_string_svalue(sp);
+        else if(ip->connection_type == PORT_TYPE_TELNET)
+            telnet_send(ip->telnet, sp->u.string, len);
+        else
+            bufferevent_write(ip->ev_buffer, sp->u.string, len);                
+        free_string_svalue(sp--);
     }
-#ifndef NO_BUFFER_TYPE
-    else {
-        if (current_object->interactive) {
-            if(current_object->interactive->connection_type == PORT_TYPE_WEBSOCKET)
-                websocket_send_text(current_object->interactive->lws, (char *)sp->u.buf->item, sp->u.buf->size);
-            else            
-                bufferevent_write(current_object->interactive->ev_buffer, (char *)sp->u.buf->item, sp->u.buf->size);
+    else if (sp->type == T_BUFFER) {
+        if(ip->connection_type == PORT_TYPE_WEBSOCKET) {
+            if(lws_get_protocol(ip->lws)->id == PROTOCOL_WS_TELNET)
+                telnet_send(ip->telnet, reinterpret_cast<char *>(sp->u.buf->item), sp->u.buf->size);
+            else
+                websocket_send_text(ip->lws, reinterpret_cast<char *>(sp->u.buf->item), sp->u.buf->size);
         }
-        free_buffer((sp)->u.buf);
+        else if(ip->connection_type == PORT_TYPE_TELNET)
+            telnet_send(ip->telnet, reinterpret_cast<char *>(sp->u.buf->item), sp->u.buf->size);
+        else            
+            bufferevent_write(ip->ev_buffer, reinterpret_cast<char *>(sp->u.buf->item), sp->u.buf->size);
+       free_buffer((sp--)->u.buf);
     }
-#endif
 }
 #endif
 
